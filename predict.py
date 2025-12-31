9283b141751eb34b18beeb7271265db8e2b5dd12
Miguel Garduno <miguelgardunom@gmail.com>
2025-12-31 10:02:55 -0600

    Prediction endpoint

--- predict.py
+++ predict.py
@@ -1,30 +1,25 @@
 import joblib 
 import pandas as pd 
+from typing import Dict, Any
+from fastapi import FastAPI
+import uvicorn
 
+app = FastAPI(title="predict")
 pipeline = joblib.load("model.pkl")
-datapoint = {
-    'gender': 'female',
-    'seniorcitizen': 0,
-    'partner': 'yes',
-    'dependents': 'no',
-    'phoneservice': 'no',
-    'multiplelines': 'no_phone_service',
-    'internetservice': 'dsl',
-    'onlinesecurity': 'no',
-    'onlinebackup': 'yes',
-    'deviceprotection': 'no',
-    'techsupport': 'no',
-    'streamingtv': 'no',
-    'streamingmovies': 'no',
-    'contract': 'month-to-month',
-    'paperlessbilling': 'yes',
-    'paymentmethod': 'electronic_check',
-    'tenure': 1,
-    'monthlycharges': 29.85,
-    'totalcharges': 29.85
-}
-X_new = pd.DataFrame([datapoint])
 
-result = round(pipeline.predict_proba(X_new)[0,1], 3)
 
-print(f"{result} prob of churning")
+def predict_single(customer):
+    X_new = pd.DataFrame([customer])
+    result = round(pipeline.predict_proba(X_new)[0,1], 3)
+    return float(result)
+
+@app.post("/predict")
+def predict(customer: Dict[str, Any]):
+    result = predict_single(customer)
+    return{
+        'churn_prob': result,
+        'churn': bool(result >= 0.5)
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=9696)
